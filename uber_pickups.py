6efd100717b2b9874bdf1791b7fa836f3a0306c3
subu873 <singhshubham873@gmail.com>
2022-11-30 00:21:16 +0530

    subheader

--- uber_pickups.py
+++ uber_pickups.py
@@ -3,3 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
